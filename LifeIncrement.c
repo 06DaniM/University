29ed30d4776067aa65de88c730628f184491cd61
06DaniM <danimyw06@gmail.com>
2024-09-20 19:00:27 +0200

    Update LifeIncrement.c

--- LifeIncrement.c
+++ LifeIncrement.c
@@ -4,11 +4,28 @@ int main()
 {
 	int playerlife;
 
-	printf("Enter your Player Life here: \n");
+	printf("Enter your Player Life here (between 0 and 100): \n");
 	scanf_s("%d", &playerlife);
 
-	float booflife = playerlife + (0.25 * playerlife);
-	printf("You recieved a potion boost life of 25%, now your have: \n%f", booflife);
+	if ((playerlife > 100) || (playerlife < 0))
+	{
+		printf("Your Player Life is not valid, learn how to read.");
+	}
 
+	else if (playerlife == 100)
+	{
+		printf("Your Player Life is full, why are you here?? ");
+	}
+
+	else if (playerlife == 0)
+	{
+		printf("You are already dead XD");
+	}
+
+	else
+	{
+		float booflife = playerlife + (0.25 * playerlife);
+		printf("You recieved a potion boost life of 25%, now your have: \n%f", booflife);
+	}
 	return 0;
 }
