0a3154804066a20739b6b913260927e374187fb3
06DaniM <danimyw06@gmail.com>
2024-10-01 10:24:42 +0200

    Update P&OEx1.c

--- P&OEx1.c
+++ P&OEx1.c
@@ -1,12 +1,30 @@
 #include <stdio.h>
 
-int main()
+void ex1()
 {
 	int sec;
 
 	printf("Give me a number of seconds and I will return the number of hours: ");
 	scanf_s("%d", &sec);
 
-	printf("%d", sec / 3600);
+	printf("%f",(float) sec / 3600);
+}
+
+int main()
+{
+	//ex1();
+
+	char c1, c2;
+
+	printf("Introduce one char: \n");
+	scanf_s("%c", &c1);
+
+	getchar();
+
+	printf("Introduce another char: \n");
+	scanf_s("%c", &c2);
+
+	printf("c1 = %c \nc2 = %c", c1, c2);
+
 	return 0;
 }
