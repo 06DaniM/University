583048aac8852e78cd4019a03418df8c306716eb
06DaniM <danimyw06@gmail.com>
2024-10-07 18:29:04 +0200

    Update UppLowerCase.c

--- UppLowerCase.c
+++ UppLowerCase.c
@@ -1,26 +1,28 @@
 #include <stdio.h>
 
-int main() {
+int main() 
+{
     char ch;
-    
+
     printf("Enter a letter: ");
-    scanf_s("%c", &ch);
+    scanf_s(" %c", &ch); 
 
     if (ch >= 'A' && ch <= 'Z') 
     {
-        ch += 32;
+        ch = ch + ('a' - 'A');
     }
 
     else if (ch >= 'a' && ch <= 'z') 
     {
-        ch += 32;
+        ch = ch - ('a' - 'A');
     }
-        
+
     else 
     {
         printf("Invalid input, please enter an alphabet letter.\n");
         return 1;
     }
+
 
     printf("Converted letter: %c\n", ch);
 
