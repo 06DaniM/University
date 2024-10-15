9f94f6ea85bed485fc121cf05375c0e887b4f7e9
06DaniM <danimyw06@gmail.com>
2024-10-15 11:07:34 +0200

    Update Pirámide.c

--- Pirámide.c
+++ Pirámide.c
@@ -1,29 +1,35 @@
 #include <stdio.h>
 
-int main()
-{
-	int maxfiles;
-	int currentfile = 1;
-	int numberperfile = 1;
+int main() {
+    int maxfiles;
+    int currentfile = 1;
+    int numberperfile;
 
-	printf("Introduce the number of files: \n");
-	scanf_s("%d", &maxfiles);
+    printf("Introduce the number of files: \n");
+    scanf_s("%d", &maxfiles);
 
-	while (maxfiles >= currentfile)
-	{
-		printf("\n");
-		while (numberperfile <= currentfile)
-		{
-			printf("%d ", numberperfile);
-			numberperfile++;
-		}
+    while (currentfile <= maxfiles) {
 
-		if (numberperfile > currentfile)
-		{
-			currentfile++;
-			numberperfile = 1;
-		}
-	}
+        int spaces = maxfiles - currentfile; 
+        int i = 0;
 
-	return 0;
+        while (i < spaces)
+        {
+            printf(" ");
+            i++;
+        }
+
+        numberperfile = 1;
+
+        while (numberperfile <= currentfile) 
+        {
+            printf("%d ", numberperfile);
+            numberperfile++;
+        }
+
+        printf("\n"); // Nueva línea después de cada fila
+        currentfile++; // Pasar a la siguiente fila
+    }
+
+    return 0;
 }
