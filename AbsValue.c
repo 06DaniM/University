fe48c639d0ebed05c19bf0fb8753083ca6f3d6c2
06DaniM <danimyw06@gmail.com>
2024-10-22 09:02:56 +0200

    Update AbsValue.c

--- AbsValue.c
+++ AbsValue.c
@@ -3,16 +3,18 @@
 int main()
 {
 	int num;
-	
-	printf("Introduce a number: ");
+
+	printf("Introduce a negative number: ");
 	scanf_s("%d", &num);
-	
+
 	if (num <= 0)
 	{
 		num *= -1;
+		printf("%d", num);
 	}
 
-	printf("%d", num);
+	else
+		printf("Number incorrect");
 
 	return 0;
 }
