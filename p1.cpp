d8a96891ef656eb8554c7609500dca577f6ad951
06DaniM <danimyw06@gmail.com>
2025-02-18 12:11:42 +0100

    Add files via upload

---
+++ p1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	vector <int> vec = { 1,7,3,2,4,7,5,8,0 };
+	int num2 = 0;
+	int num3 = 0;
+
+	for (int i = 0; i < vec.size(); i++)
+	{
+		if (vec[i] == vec[8])
+		{
+			num2++;
+			if (num2 > num3)
+			{
+				num3 = num2;
+			}
+		}
+	}
+	cout << num3 << endl;
+	return 0;
+}
