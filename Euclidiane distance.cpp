44518c24cca2587a37ce5748887c9c23f652ecf2
06DaniM <danimyw06@gmail.com>
2025-05-20 11:50:03 +0200

    Add files via upload

---
+++ Euclidiane distance.cpp
@@ -0,0 +1,28 @@
+﻿#include <vector>
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+double distanciaEuclidiana(vector<int> punt1, vector<int> punt2)
+{
+	float distance = 0;
+	for (int i = 0; i < punt1.size(); i++)
+	{
+		float res = 0;
+		res = (punt1[i] - punt2[i]);
+		res *= res;
+		distance += res;
+	}
+	distance = sqrt(distance);
+	return distance;
+}
+
+int main()
+{
+	vector<int> punt1 = { 2,5 };
+	vector<int> punt2 = { 5,6 };
+
+	cout << distanciaEuclidiana(punt1, punt2) << endl;
+
+	return 0;
+}
